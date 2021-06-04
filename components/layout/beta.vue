109e7d62fb75e2486bcb60e818259759aa1aa908
luis daniel rovira <luisdanielrovira8@gmail.com>
2021-06-04 05:04:12 +0200

    beta

---
+++ components/layout/beta.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="beta">
+        Este sitio web se encuentra en estado <b>(Beta)</b>, los datos y funcionalidades no son reales, contacta directamente con ekambia si necesitas hacer alguna operacion.
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class footerx extends Vue {
+  
+}
+</script>
+<style lang="sass">
+.beta
+    position: fixed
+    bottom: 0px
+    z-index: 90000
+    width: 100%
+    padding: 20px
+    background: #000
+    color: #fff
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="content">
+    <beta/>
     <navbar />
     <!-- <banner-prices /> -->
     <inicio />
